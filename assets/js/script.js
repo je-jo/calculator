4f09b10d47a962c4d4cf2188a94f873fa48f41be
Jelena Jovicic <jejovicic@gmail.com>
2022-03-09 16:13:27 +0100

    Add floats and backspace

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,20 @@
 function add(a, b) { return a + b };
 function substract(a, b) { return a - b };
 function multiply(a, b) { return a * b };
-function divide(a, b) { return a / b }
+function divide(a, b) { return a / b };
 
 function operate(operator, a, b) {
     if (operator === "+") {
-        return add(a, b)
+        return add(a, b);
     } else if (operator === "-") {
-        return substract(a, b)
+        return substract(a, b);
     } else if (operator === "*") {
-        return multiply(a, b)
+        return multiply(a, b);
     } else if (operator === "/") {
-        return divide(a, b)
+        return divide(a, b);
+    } else if (operator === "=") {
+
+        return a;
     }
 }
 
@@ -25,16 +28,18 @@ function clearAll() {
     result = 0;
     myNumber = "";
     resultDisplay.textContent = result;
-    inputDisplay.textContent = "";
+    inputDisplay.textContent = result;
 }
 
 const btnClear = document.querySelector(".clear");
 btnClear.addEventListener("click", clearAll);
 
 
+
+
 const digits = document.querySelectorAll(".digit");
 digits.forEach(digit => {
-    digit.addEventListener("click", makeNumber)
+    digit.addEventListener("click", populateDisplay)
 });
 
 const operators = document.querySelectorAll(".operator");
@@ -42,29 +47,52 @@ operators.forEach(operator => {
     operator.addEventListener("click", calculate)
 });
 
-function makeNumber(e) {
-    myNumber = myNumber.concat(e.currentTarget.textContent);
-    inputDisplay.textContent += +e.currentTarget.textContent;
+function populateDisplay(e) {
+    /* if (e.currentTarget.value === "0." && myNumber.includes(".")) {
+        return
+     } */
+    myNumber += e.currentTarget.value;
+    if (result && operator) {
+        inputDisplay.textContent = `${result} ${operator} ${parseFloat(myNumber)}`;
+    } else {
+        inputDisplay.textContent = parseFloat(myNumber);
+    }
+    resultDisplay.textContent = "=";
 }
+
+const dot = document.querySelector(".decimal");
+dot.addEventListener("click", makeFloat)
+
+function makeFloat() {
+    myNumber += ".";
+    //myNumber = parseFloat(myNumber);
+}
+
+const btnBackspace = document.querySelector(".backspace");
+btnBackspace.addEventListener("click", function () {
+    myNumber = myNumber.slice(0, -1);
+});
 
 function calculate(e) {
+    resultDisplay.textContent = parseFloat(myNumber);
     if (result === 0) {
-        result = Number(myNumber);
+        result = parseFloat(myNumber);
     } else {
-        result = operate(operator, result, Number(myNumber));
+        result = operate(operator, result, parseFloat(myNumber));
     }
-    inputDisplay.textContent = ""
-    inputDisplay.textContent += `${result} ${e.currentTarget.textContent} `;
-    resultDisplay.textContent = result;
+    if (e.currentTarget.textContent !== "=") {
+        inputDisplay.textContent = "";
+        inputDisplay.textContent += `${result} ${e.currentTarget.textContent} `;
+    }
+    if (result === Infinity) {
+        resultDisplay.textContent = `Nope!`;
+        result = 0;
+    } else {
+        resultDisplay.textContent = `= ${result}`;
+    }
     myNumber = "";
     operator = e.currentTarget.textContent;
+    console.log(`result is ${result} operator is ${operator} mynymber is ${myNumber}`)
 }
-
-const equals = document.querySelector(".equals");
-equals.addEventListener("click", function (e) {
-    result = operate(operator, result, Number(myNumber));
-    inputDisplay.textContent += ` ${e.currentTarget.textContent} `;
-    resultDisplay.textContent = result;
-})
 
 clearAll();
