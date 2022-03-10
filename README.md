b696db702d61b104a55e0f6ed42845880435fd71
Jelena Jovicic <jejovicic@gmail.com>
2022-03-10 14:19:59 +0100

    Add readme, fix bugs, start keyboard support

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # calculator
-TOP Project #4
+# The Odin Project - Calculator
+
+Simple on-screen calculator using JavaScript, HTML, and CSS. Built for [The Odin Project](https://www.theodinproject.com).
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Description](#description)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [The process](#the-process)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### Description
+
+This is a simple calculator that performs basic math operations on a pair of numbers.
+
+### Screenshot
+
+![](calculator.png)
+
+### Links
+
+- [Solution](https://github.com/je-jo/calculator)
+- [Live Site](https://je-jo.github.io/calculator/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- CSS Grid
+- Mobile-first workflow
+- Javascript
+
+### The process
+
+As we've been warned, getting useful variables from user input, was the hardest part of this project. My solution is to store a single number by building a string from the input, than turn that string to number as a second argument for calculations, when clicking an operator button. The first argument is a previous result, or a 0.
+
+- [x] Create functions for basic math calculations;
+- [x] Create operate() function that calls a calculation function;
+- [x] Create basic html calculator with display, digits, operators and "clear" buttons;
+- [x] Create function to populate display when clicking on numbers;
+- [x] Make calculator work: store input numbers and operator, perform calculations and update display with result;
+- [x] Add ability to string together several operations;
+- [x] Round results with long decimals;
+- [x] Display error message on division by zero;
+- [x] Extra credit: Add "." button;
+- [x] Extra credit: Add "backspace" button;
+- [] Extra credit: Make calculator pretty;
+- [] Extra credit: Add keyboard support;
+
+
+### Useful resources
+
+- [Minireset](https://awesomeopensource.com/project/jgthms/minireset.css?categoryPage=29) - A tiny modern CSS reset. 
+- [Box Shadows](https://box-shadow.dev/) A fun and useful tool for playing around with box shadows.
+
+## Author
+
+- [Github](https://github.com/je-jo)
+- [Frontend Mentor](https://www.frontendmentor.io/profile/je-jo)
+- [Codepen](https://codepen.io/je-jo)
+- [Twitter](https://twitter.com/jelena_jo_)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
-function add(a, b) { return a + b };
-function substract(a, b) { return a - b };
-function multiply(a, b) { return a * b };
-function divide(a, b) { return a / b };
+const add = (a, b) => a + b;
+const substract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
 
 function operate(operator, a, b) {
-    if (operator === "+") {
-        return add(a, b);
-    } else if (operator === "-") {
-        return substract(a, b);
-    } else if (operator === "*") {
-        return multiply(a, b);
-    } else if (operator === "/") {
-        return divide(a, b);
-    } else if (operator === "=") {
-
-        return a;
+    switch (operator) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return substract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+        default:
+            return a;
     }
 }
 
@@ -24,75 +24,121 @@ let result;
 let myNumber;
 let operator;
 
+// clear all
+
+const btnClear = document.querySelector(".clear");
+btnClear.addEventListener("click", clearAll);
+
 function clearAll() {
-    result = 0;
+    result = null;
     myNumber = "";
     resultDisplay.textContent = result;
     inputDisplay.textContent = result;
 }
 
-const btnClear = document.querySelector(".clear");
-btnClear.addEventListener("click", clearAll);
-
-
-
+// get number input and store it
 
 const digits = document.querySelectorAll(".digit");
 digits.forEach(digit => {
-    digit.addEventListener("click", populateDisplay)
+    digit.addEventListener("click", buildString);
 });
 
-const operators = document.querySelectorAll(".operator");
-operators.forEach(operator => {
-    operator.addEventListener("click", calculate)
-});
+function buildString(e) {
+    /* console.log(e)
+    console.log(e.type)
+    console.log(e.key) */
+    myNumber = myNumber.toString();
+    if (e.type === "click") {
+        myNumber += e.currentTarget.value;
+    } else {
+        myNumber += e.key
+    }
+    updateDisplay();
+}
 
-function populateDisplay(e) {
-    /* if (e.currentTarget.value === "0." && myNumber.includes(".")) {
-        return
-     } */
-    myNumber += e.currentTarget.value;
+function updateDisplay() {
     if (result && operator) {
-        inputDisplay.textContent = `${result} ${operator} ${parseFloat(myNumber)}`;
+        inputDisplay.textContent = `${result} ${operator} ${myNumber}`;
     } else {
         inputDisplay.textContent = parseFloat(myNumber);
     }
     resultDisplay.textContent = "=";
 }
 
-const dot = document.querySelector(".decimal");
-dot.addEventListener("click", makeFloat)
+// decimal button
 
-function makeFloat() {
-    myNumber += ".";
-    //myNumber = parseFloat(myNumber);
+const dot = document.querySelector(".decimal");
+dot.addEventListener("click", addDecimal);
+
+function addDecimal() {
+    myNumber = myNumber.toString();
+    if (myNumber.includes(".")) {
+        return
+    } else {
+        myNumber += ".";
+        inputDisplay.textContent += ".";
+    }
 }
 
+// backspace button
+
 const btnBackspace = document.querySelector(".backspace");
-btnBackspace.addEventListener("click", function () {
-    myNumber = myNumber.slice(0, -1);
+btnBackspace.addEventListener("click", deleteCharacter)
+
+function deleteCharacter() {
+    myNumber = myNumber.toString();
+    if (myNumber.length > 1) {
+        myNumber = myNumber.slice(0, -1);
+    } else {
+        myNumber = 0;
+    }
+    updateDisplay();
+}
+
+// do calculations 
+
+const operators = document.querySelectorAll(".operator");
+operators.forEach(operator => {
+    operator.addEventListener("click", calculate);
 });
 
 function calculate(e) {
-    resultDisplay.textContent = parseFloat(myNumber);
-    if (result === 0) {
-        result = parseFloat(myNumber);
+    console.log(operator)
+    if (result === null && myNumber === "") {
+        return
+    }
+    else if (operator && myNumber === "") { //prevents errors when clicking operator btns multiple times
+        operator = e.currentTarget.textContent; //just use the last one clicked
+        updateDisplay();
     } else {
-        result = operate(operator, result, parseFloat(myNumber));
+        myNumber = parseFloat(myNumber);
+        resultDisplay.textContent = myNumber;
+        if (result === Infinity) {  //if divide by zero clear all
+            alert(`Nope! Starting over.`);
+            clearAll();
+        } else if (result === null) {
+            result = myNumber; //stores input as first number
+        } else {
+            result = operate(operator, result, myNumber); //when the 1.num is already stored, do calculation with input as 2.num.
+            resultDisplay.textContent = `= ${+result.toFixed(3)}`;
+        }
+        myNumber = "";
+        operator = e.currentTarget.textContent; //set operator after calculation, because of chaining.
+        if (operator !== "=") {
+            inputDisplay.textContent += ` ${operator}`;
+        }
     }
-    if (e.currentTarget.textContent !== "=") {
-        inputDisplay.textContent = "";
-        inputDisplay.textContent += `${result} ${e.currentTarget.textContent} `;
-    }
-    if (result === Infinity) {
-        resultDisplay.textContent = `Nope!`;
-        result = 0;
-    } else {
-        resultDisplay.textContent = `= ${result}`;
-    }
-    myNumber = "";
-    operator = e.currentTarget.textContent;
-    console.log(`result is ${result} operator is ${operator} mynymber is ${myNumber}`)
+
 }
 
 clearAll();
+
+/*window.addEventListener("keydown", function(e) {
+    if (/\d/.test(e.key)) {
+        buildString()
+    }
+}); */
+
+window.addEventListener("keydown", buildString)
+
+
